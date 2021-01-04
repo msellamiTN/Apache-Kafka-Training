9eef0ebe4a4608669b54eb545928e11fa4ab0aa4
msellamiTN <mokhtar.sellami@gmail.com>
2021-01-04 16:50:15 +0100

    Update create-topics.sh

--- kafka/scripts/create-topics.sh
+++ kafka/scripts/create-topics.sh
@@ -1,11 +1,14 @@
 echo "Waiting for Kafka to come online..."
 
+
 cub kafka-ready -b kafka:9092 1 20
+
+sleep 60
 
 # create the users topic
 kafka-topics \
   --bootstrap-server kafka:9092 \
-  --topic users \
+  --topic mokhtar \
   --replication-factor 1 \
   --partitions 4 \
   --create
